41e88d84eab719b40d3beb214b475e226d441b2c
Oppey <ishantgouratwork@gmail.com>
2025-11-10 22:16:05 +0530

    Refactor span ref assignment in TextPressure
    Updated the ref callback for span elements to use a block body, improving readability and maintainability.

--- app/components/TextPressure.tsx
+++ app/components/TextPressure.tsx
@@ -228,7 +228,9 @@ const TextPressure = ({
         {chars.map((char, i) => (
           <span
             key={i}
-            ref={el => (spansRef.current[i] = el)}
+            ref={el => {
+              spansRef.current[i] = el;
+            }}
             data-char={char}
             style={{
               display: 'inline-block',
